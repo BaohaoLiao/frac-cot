fa9d699ac302a117ce6ee6e51c5a52393d61a6cf
baohaoliao <liaobaohao@gmail.com>
2025-05-21 20:04:26 +0200

    passk

--- scripts/passk.sh
+++ scripts/passk.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -x
 
+INPUT_FILE=
+
 echo "pass@k in single dimension"
 
 # n is also the vanilla sampling
 for DIM in n H; do
 
 python3 -u passk_single_dim.py \
-    --input_file  \
+    --input_file  ${INPUT_FILE} \
     --dimension ${DIM} \
     --k_values "1,2,4,8,16"
 
@@ -16,7 +18,7 @@ done
 for DIM in m; do
 
 python3 -u passk_single_dim.py \
-    --input_file  \
+    --input_file ${INPUT_FILE} \
     --dimension ${DIM} \
     --k_values "1,2,4"
 
@@ -25,7 +27,7 @@ done
 echo "pass@k in multiple dimensions"
 
 python3 -u passk_multi_dim.py \
-    --input_file  \
+    --input_file ${INPUT_FILE} \
     --k_values "1,2,4,8,16" \
     --h_chunks 16 \
     --m_solutions 4 \
